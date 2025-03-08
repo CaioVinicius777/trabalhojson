9bde3959ef7bff3ee0e831327aa6684fbb2ce9e6
CaioVinicius777 <caio.soares48@etec.sp.gov.br>
2025-03-07 21:26:40 -0300

    first commit

--- processar.php
+++ processar.php
@@ -1,14 +1,22 @@
 <?php
 
+// Recebendo os dados do formulário
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 $email = $_POST['email'];
 $cidade = $_POST['cidade'];
-$matematica = $_POST['matematica'];
-$portugues = $_POST['portugues'];
-$historia = $_POST['historia'];
 
-// Inclui a classe do aluno
+// Recebendo as notas e transformando-as em valores numéricos
+$matematica = floatval($_POST['matematica']);
+$portugues = floatval($_POST['portugues']);
+$historia = floatval($_POST['historia']);
+
+// Exibir os valores recebidos (para depuração)
+echo "Matematica: $matematica<br>";
+echo "Portugues: $portugues<br>";
+echo "Historia: $historia<br>";
+
+// Inclui a classe do aluno (certifique-se de que esta classe existe e está funcionando)
 include "aluno.php";
 
 // Criamos um objeto do tipo Aluno
@@ -23,6 +31,11 @@ $aluno->matematica_status = ($matematica >= 6) ? "Aprovado" : "Reprovado";
 $aluno->portugues_status = ($portugues >= 6) ? "Aprovado" : "Reprovado";
 $aluno->historia_status = ($historia >= 6) ? "Aprovado" : "Reprovado";
 
+// Exibir o status (para depuração)
+echo "Matematica Status: {$aluno->matematica_status}<br>";
+echo "Portugues Status: {$aluno->portugues_status}<br>";
+echo "Historia Status: {$aluno->historia_status}<br>";
+
 // Transforma o objeto em JSON
 $json = json_encode($aluno, JSON_PRETTY_PRINT);
 
@@ -34,4 +47,3 @@ header("Location: formulario.html");
 exit();
 
 ?>
-
